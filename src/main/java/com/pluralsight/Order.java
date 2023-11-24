0451743a62d213c7c1e2fa49aa00dcb5152303c6
Shankar Ale Magar <shankaralemagar91@gmail.com>
2023-11-23 21:03:53 -0500

    Added some things

--- src/main/java/com/pluralsight/Order.java
+++ src/main/java/com/pluralsight/Order.java
@@ -7,9 +7,27 @@ import com.pluralsight.OrderTypeListFolder.Sandwich.Sandwich;
 import java.util.ArrayList;
 
 public class Order {
-    ArrayList<Sandwich> sandwiches = new ArrayList<>();
+    ArrayList<Sandwich> sandwiches = new ArrayList<Sandwich>();
     ArrayList<Drink> drinks = new ArrayList<Drink>();
     ArrayList<Chip> chips = new ArrayList<Chip>();
+
+
+    public void addSandwich(Sandwich sandwich)
+    {
+       this.sandwiches.add(sandwich);
+
+    }
+    public void addDrink(Drink d)
+    {
+        this.addDrink(d);
+
+    }
+    public void addChips(Chip c)
+    {
+        this.addChips(c);
+    }
+
+
 
 
 }
--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -2,6 +2,8 @@ package com.pluralsight;
 
 public class UserInterface {
 
+
+
     public void displayHomeScreen()
     {
         // Greet the customer and give customer option to select from make an order or exit
@@ -10,6 +12,8 @@ public class UserInterface {
     public void displayOrderScreen()
     {
         //Ask the customer to choose the option for Sandwich, Drinks, chips or checkout or Cancel Order
+
+
     }
     public void displaySandwichScreen()
     {
@@ -37,11 +41,13 @@ public class UserInterface {
 
     public void displayCheckoutScreen(Order order)
     {
-
+      //display the checkout Screen
+        //Ask customer to proceed with the transaction or cancel the order
     }
 
     public void displayReceipt(Receipt receipt)
     {
+        //After Checkout show the receipt and Add the receipt in the file
 
     }
 }
